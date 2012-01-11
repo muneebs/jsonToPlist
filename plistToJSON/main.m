d2b8375e350b0d4681517825c7d6e8427cc1585e
Craig Spitzkoff <spitz@Raizlabs.com>
2012-01-11 10:57:35 -0500

    Initial Commit

---
+++ plistToJSON/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  plistToJSON
+//
+//  Created by Craig Spitzkoff on 1/11/12.
+//  Copyright (c) 2012 Raizlabs Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
