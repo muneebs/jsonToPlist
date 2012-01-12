14a0db1c4d98a9f3babfe903f93970f6fbc01be2
Craig Spitzkoff <spitz@Raizlabs.com>
2012-01-12 09:49:40 -0500

    Initial commit

--- plistToJSON/main.m
+++ plistToJSON/main.m
@@ -13,8 +13,58 @@ int main (int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        if(argc < 2)
+        {
+            printf("Not enough arguments; missing plist file.\n");
+            return 1;
+        }
+        
+        NSFileManager *filemgr;
+        NSString *currentpath;
+        
+        filemgr = [[NSFileManager alloc] init];
+        
+        currentpath = [filemgr currentDirectoryPath];
+        
+        // try to find the full path to the spcified file.
+        NSString* path = [NSString stringWithCString:argv[1] encoding:NSUTF8StringEncoding];
+        if(![filemgr fileExistsAtPath:path])
+        {
+            // path was not absolute or did not exist. Try looking in the current path. 
+            path = [currentpath stringByAppendingPathComponent:path];
+            
+            if(![filemgr fileExistsAtPath:path])
+            {
+                printf("Could not find file %s\n", argv[1]);
+                return 1;
+            }
+        }
+        
+        
+        NSDictionary* dictionary = [NSDictionary dictionaryWithContentsOfFile:path];
+        if (nil == dictionary) {
+            printf("Could not parse plist file %s\n", argv[1]);
+            return 1;
+        }
+        
+        
+        NSError* error = nil;
+        
+        NSData* data = [NSJSONSerialization dataWithJSONObject:dictionary options:NSJSONWritingPrettyPrinted error:&error];
+        
+        if(nil == data)
+        {
+            NSString* errorMessage = [NSString stringWithFormat:@"There was an error converting to JSON: %@", error];
+            printf("%s", [errorMessage cStringUsingEncoding:NSUTF8StringEncoding]);
+            return 1;
+        }
+        
+        path = [path stringByAppendingPathExtension:@"json"];
+        
+        if (![data writeToFile:path atomically:YES]) {
+            printf("Error writing to file: %s", [path cStringUsingEncoding:NSUTF8StringEncoding]);
+            return 1;
+        }
         
     }
     return 0;
